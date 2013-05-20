ccf63cd7be1499f46a5fdff6ed91fcb64a6c7b8d
beije <git@benjaminhorn.se>
2013-05-20 22:49:27 +0200

    Styling and animation changed lines to semi-transparent blue and made the dots only connect as pairs (for a more chaotic look)

--- monorun/clientscript/core.js
+++ monorun/clientscript/core.js
@@ -15,25 +15,21 @@ var core = {
 		this.painter = new painter( '#game' );
 		this.lineHandler = new Line($( '#game' )[0] );
 		this.lineHandler.setLineWidth( 10 );
-		this.lineHandler.setLineColor( '#1d2c31' );
+		this.lineHandler.setLineColor( 'rgba(87,197,219,0.1)' );
 
 		this.setupEvents();
 		this.resizeCanvas();
 		console.log( this.painter )
 	},
 	connectingRoland: function() {
-		var oldPos = this.enemies[ this.enemies.length-1 ].getPositions();
-		var newPos = {};
-		for( var i = 0; i < this.enemies.length; i++ ) {
+		for( var i = 0; i < this.enemies.length; i = i + 2 ) {
+			if( !this.enemies[i+1] ) break;
 			var basePos = this.enemies[i].getPositions();
-			for( var n = 1; n < 4; n++ ) {
-				if( this.enemies[i+n] ) {
-					var endPos = this.enemies[i+n].getPositions();
-					this.lineHandler.setStartPosition( basePos.x+30, basePos.y+30 );
-					this.lineHandler.setEndPosition( endPos.x+30, endPos.y+30 );
-					this.lineHandler.getLine();
-				}
-			}
+			var newPos = this.enemies[i+1].getPositions();
+			this.lineHandler.setStartPosition( basePos.x+30, basePos.y+30 );
+			this.lineHandler.setEndPosition( newPos.x+30, newPos.y+30 );
+			this.lineHandler.getLine();
+			oldPos = basePos;
 		}
 
 	},
